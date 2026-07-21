d3e8a5ef1495006e186d7115d7bc8b568e7dbb22
rx <rx@localhost>
2026-07-21 11:35:42 +0800

    Prepare G1 real robot deployment

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ from setuptools import setup, find_packages
 # Minimum dependencies required prior to installation
 INSTALL_REQUIRES = [
     "mjlab==1.2.0",
+    "mujoco==3.5.0",
     "mujoco-warp==3.5.0",
+    "scipy",
+    "warp-lang==1.12.1",
 ]
 
 # Installation operation
